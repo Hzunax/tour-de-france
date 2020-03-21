9d5c2e1bf10a83135d2dbf36d5d6b59679286ef8
Stijn Van Asschodt <stijn.van.asschodt@ing.com>
2020-03-21 20:27:03 +0100

    feat(timer): added inputs for random timer

--- src/components/RandomTimer.js
+++ src/components/RandomTimer.js
@@ -1,19 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, IconButton, TextField } from "@material-ui/core";
+import StopIcon from "@material-ui/icons/Stop";
+import './styles/RandomTimer.css';
+
+const ringtone = new Audio("/rodania.mp3");
+let timerId;
 
 function RandomTimer() {
-  let ringtone = new Audio("/rodania.mp3");
+  const [minTime, setMinTime] = useState(0);
+  const [maxTime, setMaxTime] = useState(0);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
 
   const startTimer = () => {
-    setTimeout(() => {
-      console.log('going to play');
+    const randomTime = Math.floor(
+      Math.random() * Math.abs(maxTime - minTime) * 1000 * 60
+    );
+
+    timerId = setTimeout(() => {
       ringtone.play();
-      console.log('playing');
-    }, Math.floor(Math.random() * 5000));
+      setIsMusicPlaying(true);
+    }, minTime * 1000 * 60 + randomTime);
+    setTimerStarted(true);
+  };
+
+  const cancelTimer = () => {
+    clearTimeout(timerId);
+    setTimerStarted(false);
+  }
+
+  const stopMusic = () => {
+    ringtone.pause();
+    ringtone.currentTime = 0;
+    cancelTimer();
+    setIsMusicPlaying(false);
   }
 
   return (
-    <div>
-      <button onClick={startTimer}>Start timer</button>
+    <div id="timer-container">
+      <div id="text-container">
+        <TextField
+          id="min-time"
+          className="timeInput"
+          label="Minimum"
+          variant="outlined"
+          type="number"
+          value={minTime}
+          onChange={event => setMinTime(event.target.value)}
+          required
+        />
+        <TextField
+          id="max-time"
+          className="timeInput"
+          label="Maximum"
+          variant="outlined"
+          type="number"
+          value={maxTime}
+          onChange={event => setMaxTime(event.target.value)}
+          required
+        />
+      </div>
+
+      <Button id="start-button" variant="contained" color="primary" onClick={startTimer}>
+        Start timer
+      </Button>
+      <Button id="cancel-button" variant="contained" disabled={!timerStarted} color="secondary" onClick={cancelTimer}>
+        Cancel timer
+      </Button>
+      <IconButton id="stop-button" aria-label="stop" disabled={!isMusicPlaying} color="secondary" onClick={stopMusic}>
+        <StopIcon />
+      </IconButton>
     </div>
   );
 }
